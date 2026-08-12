7bd600dd54bfb38eec5c54b635a2f34de58a30fe
juliettedesbiez <juliette.desbiez@gmail.com>
2026-08-12 12:22:11 +0100

    Update extract_features_organoid_17.py

--- extract_features_organoid_17.py
+++ extract_features_organoid_17.py
@@ -26,7 +26,7 @@ import yaml
 from scipy.stats import skew, kurtosis
 from utils_organoid import RANDOM_STATE
  
-BASE = r"C:\Users\julie\OneDrive - Imperial College London\organoid data output"
+BASE = r"C:\Users\julie\OneDrive - Imperial College London\organoid data output retrain 3"
  
 def load_config(path="fscv_config_organoid.yaml"):
     with open(path, 'r') as f:
